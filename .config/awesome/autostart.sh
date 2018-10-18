a1b33a49ae40c4997d820162017d5241ed7e58dc
LeD3F <38860726+LeD3F@users.noreply.github.com>
2018-10-18 08:47:41 +0300

    Update autostart.sh

--- .config/awesome/autostart.sh
+++ .config/awesome/autostart.sh
@@ -14,6 +14,3 @@ run setxkbmap -layout "us,ru" -option "grp:alt_shift_toggle"
 run /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 & eval $(gnome-keyring-daemon -s --components=pkcs11,secrets,ssh,gpg) &
 xrandr --output DVI-I-1 --mode 1440x900 -r 75 --pos 0x180 --rotate normal --output DP-2 --primary --mode 1920x1080 -r 144 --pos 1440x0 --rotate normal
 run pamac-tray
-run xset s off
-run xset -dpms
-run xset s noblank
