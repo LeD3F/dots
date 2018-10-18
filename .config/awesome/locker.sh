d6e3e8f885953057df9836ccb98897d276c8a9fa
LeD3F <38860726+LeD3F@users.noreply.github.com>
2018-10-18 08:14:03 +0300

    Delete locker.sh

--- .config/awesome/locker.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-exec xautolock -detectsleep -time 15 -locker "i3lock-fancy-dualmonitor -pf Roboto -- scrot -z" -notify 30 -notifier "notify-send -u critical -t 10000 -- 'LOCKING screen in 30 seconds'"
-exec xset dpms 960
