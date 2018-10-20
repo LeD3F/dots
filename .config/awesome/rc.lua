b45da4c480bb58baf2b845410dde0b9aa3562c2f
LeD3F <38860726+LeD3F@users.noreply.github.com>
2018-10-20 11:12:10 +0300

    Update rc.lua

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -463,8 +463,8 @@ globalkeys = my_table.join(
               --{description = "dropdown application", group = "launcher"}),
 
     -- Dropdown application
-    awful.key({ modkey, }, "z", function () awful.screen.focused().quake:toggle() end,
-              {description = "dropdown application", group = "launcher"}),              
+    --awful.key({ modkey, }, "z", function () awful.screen.focused().quake:toggle() end,
+              --{description = "dropdown application", group = "launcher"}),              
 
     -- Widgets popups
     awful.key({ altkey, }, "c", function () if beautiful.cal then beautiful.cal.show(7) end end,
