041dd9152c87665056fdc74e072d885ba2b38992
LeD3F <38860726+LeD3F@users.noreply.github.com>
2018-12-01 08:29:04 +0300

    Create bspwm.sh

---
+++ bspwm/polybar/bspwm.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Terminate already running bar instances
+killall -q polybar
+killall -q tint2
+
+# DP-2 Bars
+polybar -c /home/led3f/.config/polybar/configbspwmdp2 nim &
+polybar -c /home/led3f/.config/polybar/configbspwmdp2 nimdes &
+polybar -c /home/led3f/.config/polybar/configbspwmdp2 nimmus &
+
+# DVI-I-1 Bars
+polybar -c /home/led3f/.config/polybar/configbspwmdvi nim &
+polybar -c /home/led3f/.config/polybar/configbspwmdvi nimdes &
+polybar -c /home/led3f/.config/polybar/configbspwmdvi nimmus &
