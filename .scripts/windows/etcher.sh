5bf22a4ad8728b690aa7c1e28e9cf68de7a12306
LeD3F <38860726+LeD3F@users.noreply.github.com>
2018-12-01 07:30:10 +0300

    Add files via upload

---
+++ .scripts/windows/etcher.sh
@@ -0,0 +1,15 @@
+#! /bin/sh
+
+wid=$1
+class=$2
+instance=$3
+title=$(xtitle "$wid")
+
+if [ "$instance" = Etcher ] ; then
+    case "$title" in
+        buddy_list|Buddy-Liste)
+            echo "floating = on"
+            xdotool windowmove $wid 0 0
+            ;;
+    esac
+fi
