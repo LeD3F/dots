327b35b4bbc4a540104c139e3f1525d188902cb5
LeD3F <38860726+LeD3F@users.noreply.github.com>
2018-12-18 00:50:15 +0300

    Update screenshotarea.sh

--- .scripts/maim/screenshotarea.sh
+++ .scripts/maim/screenshotarea.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 NAME=~/Pictures/Screenshots/$(date +%s).png
-maim --hidecursor -s $NAME
+maim -u -s $NAME
 notify-send 'DYNAMIC ENTRY!' "Picture saved at ~/Pictrues/Screenshots" --icon=$HOME/Pictures/Icons/screen.png
