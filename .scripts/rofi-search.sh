3bf780f82a087b8595d5cf8bcae24f26ac45be98
LeD3F <38860726+LeD3F@users.noreply.github.com>
2018-12-31 11:26:53 +0300

    Update rofi-search.sh

--- .scripts/rofi-search.sh
+++ .scripts/rofi-search.sh
@@ -3,7 +3,7 @@
 
 
 URL='https://duckduckgo.com/?q='
-QUERY=$(cat ~/.cache/search_history | rofi -lines 3 -theme ~/.cache/wal/flat-red.rasi -dmenu -i -p  "search:")
+QUERY=$(xkb-switch -s us && cat ~/.cache/search_history | rofi -lines 3 -theme ~/.cache/wal/flat-red.rasi -dmenu -i -p  "search:")
 if [ -n "$QUERY" ]; then
     grep -q "$QUERY"  "$HOME/.cache/search_history"
     echo $QUERY >> ~/.cache/search_history_temp && sort -u ~/.cache/search_history_temp > ~/.cache/search_history
