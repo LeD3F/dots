701954ef8df93cb07dba8dcf13b160b2bf4fb074
LeD3F <38860726+LeD3F@users.noreply.github.com>
2021-08-01 05:50:16 +0300

    Update roficlip.sh

--- .scripts/roficlip.sh
+++ .scripts/roficlip.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 
-xkb-switch -s us && rofi -modi "clipboard:greenclip print" -show clipboard -run-command '{cmd}' \
-    -lines 3 -theme ~/.cache/wal/flat-red.rasi
+xkb-switch -s us && rofi rofi -modi "clipboard:greenclip print" -show clipboard -run-command '{cmd}' -theme ~/.config/rofi/clipboard.rasi  
