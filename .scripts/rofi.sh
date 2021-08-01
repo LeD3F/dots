b0a8cb08ffb0a12af8ffd866ac6999fe52d0ec6d
LeD3F <38860726+LeD3F@users.noreply.github.com>
2021-08-01 05:50:01 +0300

    Update rofi.sh

--- .scripts/rofi.sh
+++ .scripts/rofi.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 
-xkb-switch -s us && rofi -show drun -modi drun -lines 3 -columns 3\
- -theme ~/.cache/wal/flat-red.rasi
+xkb-switch -s us && rofi -no-config -no-lazy-grab -show drun -modi drun -theme ~/.config/rofi/launcher.rasi
