7a65791c19ca4056328d0c5c0c88438a18f9ceaf
Nikita <38860726+LeD3F@users.noreply.github.com>
2021-08-04 03:19:17 +0300

    Add files via upload

--- herbstluftwm/polybar/herb.sh
+++ herbstluftwm/polybar/herb.sh
@@ -2,12 +2,10 @@
 
 # Terminate already running bar instances
 killall -q polybar
-killall -q tint2
 
 # Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar1 and bar2
-polybar -c ~/.config/polybar/herb/config nim &
-polybar -c ~/.config/polybar/herb/config nim2 &
+# DP-2 Bars
+polybar nopa -c ~/.config/polybar/config &
 
-echo "Bars launched..."
