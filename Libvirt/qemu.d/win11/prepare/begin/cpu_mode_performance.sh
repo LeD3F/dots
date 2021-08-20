8761a4e482ac096074c5f5ffd28e41663c25613e
Nikita <38860726+LeD3F@users.noreply.github.com>
2021-08-20 16:38:03 +0300

    Add files via upload

---
+++ Libvirt/qemu.d/win11/prepare/begin/cpu_mode_performance.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+## Enable CPU governor performance mode
+cat /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
+for file in /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor; do echo "performance" > $file; done
+cat /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
