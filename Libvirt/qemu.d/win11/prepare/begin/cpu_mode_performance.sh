f6b1d70ca51bf0f01a86313b44a43cf42f02647c
Nikita <38860726+LeD3F@users.noreply.github.com>
2021-08-22 18:42:09 +0300

    Delete Libvirt directory

--- Libvirt/qemu.d/win11/prepare/begin/cpu_mode_performance.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-## Enable CPU governor performance mode
-cat /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
-for file in /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor; do echo "performance" > $file; done
-cat /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
