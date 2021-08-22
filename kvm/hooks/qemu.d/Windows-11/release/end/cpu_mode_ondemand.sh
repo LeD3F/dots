e3b77a68474ef5c73c5e88bfb42f9f4fb89837b4
Nikita <38860726+LeD3F@users.noreply.github.com>
2021-08-22 18:41:56 +0300

    Add files via upload

---
+++ kvm/hooks/qemu.d/Windows-11/release/end/cpu_mode_ondemand.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+## Enable CPU governor on-demand mode
+cat /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
+for file in /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor; do echo "ondemand" > $file; done
+cat /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
