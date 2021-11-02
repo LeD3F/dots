1b4fb83f3bfcce225eb14f59fd86e90df8c139bb
Nikita <38860726+LeD3F@users.noreply.github.com>
2021-11-02 03:53:35 +0300

    Update blocks.def.h

--- dwmconf/dwmblocks/blocks.def.h
+++ dwmconf/dwmblocks/blocks.def.h
@@ -1,8 +1,8 @@
 static const Block blocks[] = {
 	// {"Icon", "Command", "update interval", "update signal"}
 	{"", "~/Misc/dwmconf/dwmblocks/scripts/packages",120,0},
-    {"", "~/Misc/dwmconf/dwmblocks/scripts/spotify.sh",1,0},
-    {"", "~/Misc/dwmconf/dwmblocks/scripts/weather",1,0},
+	{"", "~/Misc/dwmconf/dwmblocks/scripts/spotify",1,0},
+	{"", "~/Misc/dwmconf/dwmblocks/scripts/weather",1,0},
 	{"", "~/Misc/dwmconf/dwmblocks/scripts/memory",1,0},
 	{"", "~/Misc/dwmconf/dwmblocks/scripts/clock",1,0},
 
