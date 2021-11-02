c923b40a341ef777cfaffdc59a3e05fc61bbea0d
Nikita <38860726+LeD3F@users.noreply.github.com>
2021-11-02 03:17:38 +0300

    Add files via upload

---
+++ dwmconf/dwmblocks/scripts/spotify.sh
@@ -0,0 +1,38 @@
+
+#!/bin/bash
+
+# Set the source audio player here.
+# Players supporting the MPRIS spec are supported.
+# Examples: spotify, vlc, chrome, mpv and others.
+# Use `playerctld` to always detect the latest player.
+# See more here: https://github.com/altdesktop/playerctl/#selecting-players-to-control
+PLAYER="spotify"
+
+# Format of the information displayed
+# Eg. {{ artist }} - {{ album }} - {{ title }}
+# See more attributes here: https://github.com/altdesktop/playerctl/#printing-properties-and-metadata
+FORMAT="^c#2E3440^^b#A3BE8C^ SP^c#abb2bf^ ^b#414753^ {{ artist }} - {{ title }}"
+
+PLAYERCTL_STATUS=$(playerctl --player=$PLAYER status 2>/dev/null)
+EXIT_CODE=$?
+
+
+
+if [ $EXIT_CODE -eq 0 ]; then
+    STATUS=$PLAYERCTL_STATUS
+fi
+
+
+if [ "$1" == "--status" ]; then
+    echo "$STATUS"
+fi
+
+if [ "$STATUS" = "Paused"  ]; then
+    playerctl --player=$PLAYER metadata --format "$FORMAT"
+elif [ "$STATUS" = ""  ]; then
+    echo "$STATUS"
+else
+    playerctl --player=$PLAYER metadata --format "$FORMAT"
+
+fi
+
