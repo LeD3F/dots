b03daa41a656671e3aaa3158875bd7546d88b87c
Nikita <38860726+LeD3F@users.noreply.github.com>
2021-11-19 10:04:41 +0300

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,16 +1,40 @@
-fish_add_path -P $HOME/bin
+#PATH
+set -e fish_user_paths
+set -U fish_user_paths $HOME/bin $fish_user_paths
+
+#Settings
 set fish_greeting
+set TERM "xterm-256color"
+set EDITOR "vim"
+
+#Manpager
+set -x MANPAGER "sh -c 'col -bx | bat -l man -p'"
 
 #Aliases
+#Bat as cat
 alias cat="bat"
-alias cp='cp -i'
+
+#Confirmation before overwriting
+alias cp="cp -i"
+alias mv='mv -i'
+alias rm='rm -i'
+
+# termbin
+alias tb="nc termbin.com 9999"
+
+#Colorized grep output
 alias grep='grep --color=auto'
+alias egrep='egrep --color=auto'
+alias fgrep='fgrep --color=auto'
+
+#Some stuff
 alias less='bat'
 alias l='exa -lahF --color=always --icons --sort=size --group-directories-first'
 alias ls='exa -lhF --color=always --icons --sort=size --group-directories-first'
 alias lst='exa -lahFT --color=always --icons --sort=size --group-directories-first'
 
-if status is-interactive
-    rxfetch
-    # Commands to run in interactive sessions can go here
-end
+#Rick Roll
+alias rick='curl -s -L http://bit.ly/10hA8iC | bash'
+
+#Rxfetch info
+rxfetch
